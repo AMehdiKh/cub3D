6a36295fd883d755ea1375519c94ab42dff7cf04
Mehdi Zoheir <mzoheir@c2r4p5.1337.ma>
2023-12-30 20:49:38 +0100

    Finished textures

--- mandatory/graphics.c
+++ mandatory/graphics.c
@@ -6,7 +6,7 @@
 /*   By: mzoheir <mzoheir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 13:31:10 by ael-khel          #+#    #+#             */
-/*   Updated: 2023/12/30 18:05:39 by mzoheir          ###   ########.fr       */
+/*   Updated: 2023/12/30 19:21:38 by mzoheir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,28 +43,11 @@ void	ft_init_mlx(t_mlx *mlx)
 		mlx_terminate(mlx->win);
 		ft_err("Error: Putting image to window failed", mlx->map_data);
 	}
-	ft_open_textures(mlx);
-	load_texture(mlx);
+	load_pngs(mlx);
 	mlx->player_data->player = mlx->map_data->character;
 	mlx->player_data->rotation_angle = ft_character_direction(mlx->map_data);
 	mlx->player_data->rotation_speed = ROT_SPEED * (M_PI / 180);
 	mlx->player_data->field_of_view = FOV_ANGLE * (M_PI / 180);
-}
-
-void	ft_open_textures(t_mlx *mlx)
-{
-	load_pngs(mlx);
-	
-	if (!mlx->no_text || !mlx->so_text || !mlx->we_text || !mlx->ea_text)
-	{
-		// mlx_delete_texture(NULL);
-		// mlx_delete_texture(mlx->no_text);
-		// mlx_delete_texture(mlx->so_text);
-		// mlx_delete_texture(mlx->we_text);
-		// mlx_delete_texture(mlx->ea_text);
-		// mlx_terminate(mlx->win);
-		ft_err("Error: Texture failed to open", mlx->map_data);
-	}
 }
 
 void	ft_render_map(t_mlx	*mlx)
@@ -74,7 +57,7 @@ void	ft_render_map(t_mlx	*mlx)
 	ft_paint_ceiling_floor(mlx);
 	ft_cast_rays(mlx, rays);
 	ft_render_walls(mlx, rays);
-	// ft_mini_map(mlx);
+	ft_mini_map(mlx);
 	ft_draw_rays(mlx, rays);
 	++i;
 }
--- mandatory/textures.c
+++ mandatory/textures.c
@@ -6,7 +6,7 @@
 /*   By: mzoheir <mzoheir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 18:26:45 by mzoheir           #+#    #+#             */
-/*   Updated: 2023/12/30 19:03:35 by mzoheir          ###   ########.fr       */
+/*   Updated: 2023/12/30 20:44:34 by mzoheir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,84 +17,31 @@ uint32_t	ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t o)
 	return (r << 24 | g << 16 | b << 8 | o);
 }
 
-void	fill_colors(uint32_t *tex_colors, mlx_texture_t *text)
-{
-    // static int 		i;
-	// static uint32_t	j;
-
-	// if ((uint32_t)i < text->bytes_per_pixel * text->height * text->width)
-	// {
-	// 	texture[j++] = ft_pixel(text->pixels[i],
-	// 			text->pixels[i + 1], text->pixels[i + 2],
-	// 			text->pixels[i + 3]);
-	// 	i += 4;
-	// }
-	// if ((uint32_t)i >= text->bytes_per_pixel * text->height * text->width)
-	// {
-	// 	i = 0;
-	// 	j = 0;
-	// }
-
-	uint32_t i = 0;
-	uint32_t j = 0;
-	
-	while (i < text->bytes_per_pixel * text->height * text->width)
-	{
-		tex_colors[j] = ft_pixel(text->pixels[i], text->pixels[i + 1], text->pixels[i + 2], text->pixels[i + 3]);
-		i += 4;
-		j++;
-	}
-}
-
-uint32_t	*fill_texture(mlx_texture_t *text)
-{
-    // int i;
-    uint32_t *text_color;
-    
-	text_color = ft_calloc(sizeof(uint32_t), text->height * text->width);
-	
-	if (!text_color)
-		return (0);
-	// i = 0;
-	// while (i < (int)(text->height * text->width))
-	// {
-	// 	fill_colors(text_color, text);
-	// 	i++;
-	// }
-	
-	fill_colors(text_color, text);
-	
-	return (text_color);
-}
-
-void	load_texture(t_mlx *mlx)
-{
-	mlx->west_color = fill_texture(mlx->we_text);
-	mlx->south_color = fill_texture(mlx->so_text);
-	mlx->north_color = fill_texture(mlx->no_text);
-	mlx->east_color = fill_texture(mlx->ea_text);
-
-	// if(!mlx->north_color)
-    //     // free all
-	// if(!mlx->south_color)
-	// 	// free all
-	// if(!mlx->east_color)
-	// 	// free all
-	// if(!mlx->west_color)
-	// 	// free all
-}
-
 void   load_pngs(t_mlx *mlx)
 {
-	mlx->so_text = mlx_load_png("Textures/dodo.png");
-	mlx->no_text = mlx_load_png("Textures/dodo.png");
-	mlx->ea_text = mlx_load_png("Textures/dodo.png");
-	mlx->we_text = mlx_load_png("Textures/dodo.png");
-
-    if(!mlx->so_text || !mlx->ea_text || !mlx->no_text || !mlx->we_text)
+	mlx->so_text = mlx_load_png(mlx->map_data->so_text);
+	if(!mlx->so_text)
     {
-        //free all
-        printf("ERROR Loading Textures!\n");
+        printf("ERROR Loading SOUTH Texture!\n");
+		exit(EXIT_FAILURE);
+    }
+	mlx->no_text = mlx_load_png(mlx->map_data->no_text);
+	if(!mlx->no_text)
+    {
+        printf("ERROR Loading NORTH Texture!\n");
+		exit(EXIT_FAILURE);
+    }
+	mlx->ea_text = mlx_load_png(mlx->map_data->ea_text);
+	if(!mlx->ea_text)
+    {
+        printf("ERROR Loading EAST Texture!\n");
+		exit(EXIT_FAILURE);
+    }
+	mlx->we_text = mlx_load_png(mlx->map_data->we_text);
+    if(!mlx->we_text)
+    {
+        printf("ERROR Loading WEST Texture!\n");
+		exit(EXIT_FAILURE);
     }
 	
 }
